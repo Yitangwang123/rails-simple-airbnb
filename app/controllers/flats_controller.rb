64021525a1db6d0af0785e0765201de4580acdf0
Yitang Wang <yitangwang0@gmail.com>
2021-07-21 20:29:57 +0800

    Add picture_url to flats

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,39 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path(@flat)
+  end
+
+  private
+
+  def set_flat
+    @id = params[:id]
+    @flat = Flat.find(@id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 4 fake flats...'
+Flat.destroy_all
+4.times do
+  flat = Flat.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    description: Faker::Company.catch_phrase,
+    price_per_night: (30..100).to_a.sample,
+    number_of_guests: (1..6).to_a.sample
+  )
+  flat.save!
+end
+puts 'Finished!'
