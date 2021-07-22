c7dffdf08f205d8de2d145b5c78e4c1a1e186318
Yitang Wang <yitangwang0@gmail.com>
2021-07-22 09:49:44 +0800

    style

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,11 @@ require 'faker'
 puts 'Creating 4 fake flats...'
 Flat.destroy_all
 4.times do
+  url_array = ['https://images.unsplash.com/photo-1460317442991-0ec209397118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80', 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=973&q=80', 'https://images.unsplash.com/photo-1512918728675-ed5a9ecdebfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80', 'https://images.unsplash.com/photo-1484606075493-62da7cd65654?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=752&q=80']
   flat = Flat.new(
     name: Faker::Restaurant.name,
     address: Faker::Address.full_address,
+    picture_url: url_array.sample,
     description: Faker::Company.catch_phrase,
     price_per_night: (30..100).to_a.sample,
     number_of_guests: (1..6).to_a.sample
